c1201236cedcf8edc1b0869334f213f6707ae1eb
Artiom_Sokolov <sokolova-net@mail.ru>
2022-06-25 21:25:52 +0300

    add Tailwind CSS in project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
 import './App.css';
+import User from './components/User';
+import Posts from './components/Posts';
+import Form from './components/Form';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello word</h1>
-    </div>
+    <div className='min-h-screen h-full w-screen bg-indigo-400'>
+            <div className='container mx-auto px-4'>
+                <header className='flex gap-20 '>
+                    <div className='w-1/3'>
+                        <h1 className='font-bold my-5'>Redux Toolkit State Change</h1>
+                        <User/>
+                    </div>
+                    <div className='w-1/3'>
+                        <h1 className='font-bold my-5'>Redux Toolkit Todo App</h1>
+                        <Form />
+                        <TodoItem />
+                    </div>
+                    <div className='w-1/3'>
+                        <h1 className='font-bold my-5'>Redux Toolkit Async Thunk</h1>
+                        <Posts />
+                    </div>
+                </header>
+            </div>
+        </div>
   );
 }
 
